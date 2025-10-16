253adf3dca2e966bfd01517aec891dc3e3d405dc
Ashutosh <being.ashutosh16.20@gmail.com>
2025-10-16 10:58:03 +0530

    UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Instagram, Facebook, MapPin, Star } from "lucide-react";
-import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { Instagram, Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
